92e6fe5b8c402c12dfad5d05e3f219b4d817948b
Ivan Capobianco <capobianco.ivan@gmail.com>
2025-01-13 18:02:58 +0100

    feat: client hello server name extension

--- src/messages/client_hello.py
+++ src/messages/client_hello.py
@@ -1,9 +1,10 @@
 from src.tls_crypto import get_32_random_bytes
 
 class ClientHello:
-    def __init__(self):
+    def __init__(self, server_name):
         self.CLIENT_VERSION = b'\x03\x03'
         self.client_random = get_32_random_bytes()
+        self.server_name = server_name
 
     @staticmethod
     def get_supported_cipher_suites():
@@ -12,3 +13,25 @@ class ClientHello:
         """
         TLS_AES_128_GCM_SHA256 = b'\x13\x01'
         return len(TLS_AES_128_GCM_SHA256).to_bytes(2) + TLS_AES_128_GCM_SHA256
+
+    def get_extension_server_name(self):
+        server_name_extension_flag = b'\x00\x00'
+        return self.__build_extension(server_name_extension_flag, self.__build_server_name())
+
+    def __build_extension(self, flag, data):
+        data_bytes = len(data).to_bytes(2)
+        # Each extension starts with a flag indicating the type of extension which has a length of 2 bytes.
+        # Then there are 2 bytes indicating how long is the data that follows.
+        # Finally, there is the data.
+        return flag + data_bytes + data
+
+    def __build_server_name(self):
+        encoded_server_hostname = self.server_name.encode('utf-8')
+
+        # bytes of hostname follows
+        encoded_server_hostname_len_bytes = (len(encoded_server_hostname)).to_bytes(2)
+
+        # list entry is type 0x00 "DNS hostname"
+        list_entry_type_dns_hostname = b'\x00'
+
+        return list_entry_type_dns_hostname + encoded_server_hostname_len_bytes + encoded_server_hostname
--- test/test_client_hello.py
+++ test/test_client_hello.py
@@ -4,16 +4,24 @@ from src.messages.client_hello import ClientHello
 
 class TestClientHello(unittest.TestCase):
     def test_should_return_client_version_tls_12(self):
-        c = ClientHello()
+        c = ClientHello("")
         expected_client_version = bytes.fromhex("""03 03""")
         self.assertEqual(c.CLIENT_VERSION, expected_client_version)
 
     def test_should_return_client_random(self):
-        c = ClientHello()
+        c = ClientHello("")
         self.assertIs(len(c.client_random), 32)
 
     def test_should_return_supported_cipher_suites(self):
-        c = ClientHello()
+        c = ClientHello("")
         supported_cipher_suites = c.get_supported_cipher_suites()
         expected_supported_cipher_suites = bytes.fromhex("""00 02 13 01""")
         self.assertEqual(supported_cipher_suites, expected_supported_cipher_suites)
+
+    def test_should_return_server_name_extension(self):
+        c = ClientHello("example.ulfheim.net")
+        server_name_extension = c.get_extension_server_name()
+        expected_server_name_extension = bytes.fromhex("""00 00 00 16 00 00 13 65 78 61 6d 70 6c 65 2e 75 6c 66 
+            68 65 69 6d 2e 6e 65 74""")
+        self.assertEqual(server_name_extension, expected_server_name_extension)
+
