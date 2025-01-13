cd3ecbd20f3202276ed7c73eabedf4db80d8e87c
Ivan Capobianco <capobianco.ivan@gmail.com>
2025-01-13 17:05:55 +0100

    docs: test comments

--- test/test_tls_crypto.py
+++ test/test_tls_crypto.py
@@ -1,4 +1,3 @@
-import binascii
 import unittest
 
 from src.tls_crypto import get_X25519_private_key, get_32_random_bytes, get_32_zero_bytes, hkdf_extract, \
@@ -119,9 +118,9 @@ class TestTLSCrypto(unittest.TestCase):
          35 e6 d6 3f 53 75 63 ef d4 62 72 90 0f 89 49 2d""")
         self.assertEqual(shared_secret, expected_shared_secret)
 
+    # https://datatracker.ietf.org/doc/html/rfc8448#page-5
+    # section: {server}  derive secret "tls13 c hs traffic"
     def test_should_return_client_secret(self):
-        # https://datatracker.ietf.org/doc/html/rfc8448#page-5
-        # section: {server}  derive secret "tls13 c hs traffic"
         handshake_secret = bytes.fromhex("""1d c8 26 e9 36 06 aa 6f dc 0a ad c1 2f 74 1b
          01 04 6a a6 b9 9f 69 1e d2 21 a9 f0 ca 04 3f be ac""")
         hello_hash = bytes.fromhex("""86 0c 06 ed c0 78 58 ee 8e 78 f0 e7 42 8c 58 ed
@@ -131,9 +130,9 @@ class TestTLSCrypto(unittest.TestCase):
          2d 8f 3b 1a 95 07 38 f5 2e 96 00 74 6a 0e 27 a5 5a 21""")
         self.assertEqual(client_secret, expected_client_secret)
 
+    # https://datatracker.ietf.org/doc/html/rfc8448#page-6
+    # section: {server}  derive secret "tls13 s hs traffic"
     def test_should_return_server_secret(self):
-        # https://datatracker.ietf.org/doc/html/rfc8448#page-6
-        # section: {server}  derive secret "tls13 s hs traffic"
         handshake_secret = bytes.fromhex("""1d c8 26 e9 36 06 aa 6f dc 0a ad c1 2f 74 1b
          01 04 6a a6 b9 9f 69 1e d2 21 a9 f0 ca 04 3f be ac""")
         hello_hash = bytes.fromhex("""86 0c 06 ed c0 78 58 ee 8e 78 f0 e7 42 8c 58 ed
@@ -143,9 +142,9 @@ class TestTLSCrypto(unittest.TestCase):
          37 b4 e9 c9 12 bc de d9 10 5d 42 be fd 59 d3 91 ad 38""")
         self.assertEqual(server_secret, expected_server_secret)
 
+    # https://datatracker.ietf.org/doc/html/rfc8448#page-11
+    # section: {server}  derive read traffic keys for handshake data
     def test_should_return_client_handshake_key(self):
-        # https://datatracker.ietf.org/doc/html/rfc8448#page-11
-        # section: {server}  derive read traffic keys for handshake data
         client_secret = bytes.fromhex("""b3 ed db 12 6e 06 7f 35 a7 80 b3 ab f4 5e
          2d 8f 3b 1a 95 07 38 f5 2e 96 00 74 6a 0e 27 a5 5a 21""")
         client_handshake_key = get_client_handshake_key(client_secret)
@@ -153,9 +152,9 @@ class TestTLSCrypto(unittest.TestCase):
          25 8d 01""")
         self.assertEqual(client_handshake_key, expected_client_handshake_key)
 
+    # https://datatracker.ietf.org/doc/html/rfc8448#page-7
+    # section: {server}  derive write traffic keys for handshake data
     def test_should_return_server_handshake_key(self):
-        # https://datatracker.ietf.org/doc/html/rfc8448#page-7
-        # section: {server}  derive write traffic keys for handshake data
         server_secret = bytes.fromhex("""b6 7b 7d 69 0c c1 6c 4e 75 e5 42 13 cb 2d
          37 b4 e9 c9 12 bc de d9 10 5d 42 be fd 59 d3 91 ad 38""")
         server_handshake_key = get_server_handshake_key(server_secret)
@@ -163,27 +162,27 @@ class TestTLSCrypto(unittest.TestCase):
          e4 03 bc""")
         self.assertEqual(server_handshake_key, expected_server_handshake_key)
 
+    # https://datatracker.ietf.org/doc/html/rfc8448#page-11
+    # section: {server}  derive read traffic keys for handshake data
     def test_should_return_client_handshake_iv(self):
-        # https://datatracker.ietf.org/doc/html/rfc8448#page-11
-        # section: {server}  derive read traffic keys for handshake data
         client_secret = bytes.fromhex("""b3 ed db 12 6e 06 7f 35 a7 80 b3 ab f4 5e
          2d 8f 3b 1a 95 07 38 f5 2e 96 00 74 6a 0e 27 a5 5a 21""")
         client_handshake_iv = get_client_handshake_iv(client_secret)
         expected_client_handshake_iv = bytes.fromhex("""5b d3 c7 1b 83 6e 0b 76 bb 73 26 5f""")
         self.assertEqual(client_handshake_iv, expected_client_handshake_iv)
 
+    # https://datatracker.ietf.org/doc/html/rfc8448#page-7
+    # section: {server}  derive write traffic keys for handshake data
     def test_should_return_server_handshake_iv(self):
-        # https://datatracker.ietf.org/doc/html/rfc8448#page-7
-        # section: {server}  derive write traffic keys for handshake data
         server_secret = bytes.fromhex("""b6 7b 7d 69 0c c1 6c 4e 75 e5 42 13 cb 2d
          37 b4 e9 c9 12 bc de d9 10 5d 42 be fd 59 d3 91 ad 38""")
         server_handshake_iv = get_server_handshake_iv(server_secret)
         expected_server_handshake_iv = bytes.fromhex("""5d 31 3e b2 67 12 76 ee 13 00 0b 30""")
         self.assertEqual(server_handshake_iv, expected_server_handshake_iv)
 
+    # https://datatracker.ietf.org/doc/html/rfc8448#page-7
+    # section: {server}  extract secret "master"
     def test_should_return_derived_secret_from_handshake_secret(self):
-        # https://datatracker.ietf.org/doc/html/rfc8448#page-7
-        # section: {server}  extract secret "master"
         handshake_secret = bytes.fromhex("""1d c8 26 e9 36 06 aa 6f dc 0a ad c1 2f 74 1b
          01 04 6a a6 b9 9f 69 1e d2 21 a9 f0 ca 04 3f be ac""")
         derived_secret = get_derived_secret(handshake_secret)
@@ -191,6 +190,8 @@ class TestTLSCrypto(unittest.TestCase):
          31 90 a6 5b 3e e2 e4 f1 2d d7 a0 bb 7c e2 54 b4""")
         self.assertEqual(derived_secret, expected_derived_secret)
 
+    # https://datatracker.ietf.org/doc/html/rfc8448#page-6
+    # section: {server}  extract secret "master"
     def test_should_return_master_secret(self):
         derived_secret = bytes.fromhex("""43 de 77 e0 c7 77 13 85 9a 94 4d b9 db 25 90 b5
          31 90 a6 5b 3e e2 e4 f1 2d d7 a0 bb 7c e2 54 b4""")
@@ -199,6 +200,8 @@ class TestTLSCrypto(unittest.TestCase):
          47 80 01 bc 4d 4c 62 79 84 d5 a4 1d a8 d0 40 29 19""")
         self.assertEqual(master_secret, expected_master_secret)
 
+    # https://datatracker.ietf.org/doc/html/rfc8448#page-10
+    # section: {server}  derive secret "tls13 c ap traffic"
     def test_should_return_client_secret_application(self):
         master_secret = bytes.fromhex("""18 df 06 84 3d 13 a0 8b f2 a4 49 84 4c 5f 8a
          47 80 01 bc 4d 4c 62 79 84 d5 a4 1d a8 d0 40 29 19""")
@@ -209,6 +212,8 @@ class TestTLSCrypto(unittest.TestCase):
          65 52 87 5a fa 0b 06 df 00 87 f7 92 eb b7 c1 75 04 a5""")
         self.assertEqual(client_secret_application, expected_client_secret_application)
 
+    # https://datatracker.ietf.org/doc/html/rfc8448#page-10
+    # section: {server}  derive secret "tls13 s ap traffic"
     def test_should_return_server_secret_application(self):
         master_secret = bytes.fromhex("""18 df 06 84 3d 13 a0 8b f2 a4 49 84 4c 5f 8a
          47 80 01 bc 4d 4c 62 79 84 d5 a4 1d a8 d0 40 29 19""")
@@ -219,9 +224,9 @@ class TestTLSCrypto(unittest.TestCase):
          50 32 82 04 b4 f4 4b fb 6b 3a 4b 4f 1f 3f cb 63 16 43""")
         self.assertEqual(server_secret_application, expected_server_secret_application)
 
+    # https://datatracker.ietf.org/doc/html/rfc8448#page-13
+    # section: {client}  derive write traffic keys for application data
     def test_should_return_client_application_key(self):
-        # https://datatracker.ietf.org/doc/html/rfc8448#page-13
-        # section: {client}  derive write traffic keys for application data
         client_secret = bytes.fromhex("""9e 40 64 6c e7 9a 7f 9d c0 5a f8 88 9b ce
          65 52 87 5a fa 0b 06 df 00 87 f7 92 eb b7 c1 75 04 a5""")
         client_application_key = get_client_application_key(client_secret)
@@ -229,9 +234,9 @@ class TestTLSCrypto(unittest.TestCase):
          3f 50 51""")
         self.assertEqual(client_application_key, expected_client_application_key)
 
+    # https://datatracker.ietf.org/doc/html/rfc8448#page-11
+    # section: {server}  derive write traffic keys for application data
     def test_should_return_server_application_key(self):
-        # https://datatracker.ietf.org/doc/html/rfc8448#page-11
-        # section: {server}  derive write traffic keys for application data
         server_secret = bytes.fromhex("""a1 1a f9 f0 55 31 f8 56 ad 47 11 6b 45 a9
          50 32 82 04 b4 f4 4b fb 6b 3a 4b 4f 1f 3f cb 63 16 43""")
         server_application_key = get_server_application_key(server_secret)
@@ -239,22 +244,20 @@ class TestTLSCrypto(unittest.TestCase):
          92 e3 56""")
         self.assertEqual(server_application_key, expected_server_application_key)
 
+    # https://datatracker.ietf.org/doc/html/rfc8448#page-13
+    # section: {client}  derive write traffic keys for application data
     def test_should_return_client_application_iv(self):
-        # https://datatracker.ietf.org/doc/html/rfc8448#page-13
-        # section: {client}  derive write traffic keys for application data
         client_secret = bytes.fromhex("""9e 40 64 6c e7 9a 7f 9d c0 5a f8 88 9b ce
          65 52 87 5a fa 0b 06 df 00 87 f7 92 eb b7 c1 75 04 a5""")
         client_application_iv = get_client_application_iv(client_secret)
         expected_client_application_iv = bytes.fromhex("""5b 78 92 3d ee 08 57 90 33 e5 23 d9""")
         self.assertEqual(client_application_iv, expected_client_application_iv)
 
+    # https://datatracker.ietf.org/doc/html/rfc8448#page-11
+    # section: {server}  derive write traffic keys for application data
     def test_should_return_server_application_iv(self):
-        # https://datatracker.ietf.org/doc/html/rfc8448#page-11
-        # section: {server}  derive write traffic keys for application data
         server_secret = bytes.fromhex("""a1 1a f9 f0 55 31 f8 56 ad 47 11 6b 45 a9
          50 32 82 04 b4 f4 4b fb 6b 3a 4b 4f 1f 3f cb 63 16 43""")
         server_application_iv = get_server_application_iv(server_secret)
         expected_server_application_iv = bytes.fromhex("""cf 78 2b 88 dd 83 54 9a ad f1 e9 84""")
         self.assertEqual(server_application_iv, expected_server_application_iv)
-
-
