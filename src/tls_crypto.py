012bfb03a368cc637d4cad7e2ce89cabc492c659
Ivan Capobianco <capobianco.ivan@gmail.com>
2025-01-13 14:38:49 +0100

    feat: generate x25519 public key

--- src/tls_crypto.py
+++ src/tls_crypto.py
@@ -2,3 +2,6 @@ from cryptography.hazmat.primitives.asymmetric.x25519 import X25519PrivateKey
 
 def get_X25519_private_key():
     return X25519PrivateKey.generate()
+
+def get_X25519_public_key(private_key: X25519PrivateKey):
+    return private_key.public_key()
--- test/test_tls_crypto.py
+++ test/test_tls_crypto.py
@@ -1,9 +1,17 @@
 import unittest
 
 from src.tls_crypto import get_X25519_private_key
+from src.tls_crypto import get_X25519_public_key
+
 from cryptography.hazmat.primitives.asymmetric.x25519 import X25519PrivateKey
+from cryptography.hazmat.primitives.asymmetric.x25519 import X25519PublicKey
 
 class TestTLSCrypto(unittest.TestCase):
     def test_should_return_x25519_private_key(self):
         private_key = get_X25519_private_key()
         self.assertTrue(isinstance(private_key, X25519PrivateKey))
+
+    def test_should_return_x25519_public_key(self):
+        private_key = get_X25519_private_key()
+        public_key = get_X25519_public_key(private_key)
+        self.assertTrue(isinstance(public_key, X25519PublicKey))
