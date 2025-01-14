fb92944a995aa29ba4b5d5ee1665ecfeacb8a741
Ivan Capobianco <capobianco.ivan@gmail.com>
2025-01-14 16:28:35 +0100

    feat: handshake finished verify data

--- src/messages/handshake_finished.py
+++ src/messages/handshake_finished.py
@@ -1,3 +1,9 @@
+from src.tls_crypto import get_hmac_sha256
+
+
 class HandshakeFinished:
     def __init__(self):
         self.HANDSHAKE_MESSAGE_TYPE_FINISHED = b'\x14'
+
+    def get_verify_data(self, finished_key, finished_hash):
+        return get_hmac_sha256(finished_hash, finished_key)
--- test/test_handshake_finished.py
+++ test/test_handshake_finished.py
@@ -1,4 +1,15 @@
 import unittest
 
+from src.messages.handshake_finished import HandshakeFinished
+
+
 class TestHandshakeFinished(unittest.TestCase):
-    pass
+    def test_should_return_verify_data(self):
+        handshake_finished = HandshakeFinished()
+        finished_key = bytes.fromhex("""00 8d 3b 66 f8 16 ea 55 9f 96 b5 37 e8 85
+                    c3 1f c0 68 bf 49 2c 65 2f 01 f2 88 a1 d8 cd c1 9f c8""")
+        finished_hash = bytes.fromhex("""edb7725fa7a3473b031ec8ef65a2485493900138a2b91291407d7951a06110ed""")
+        verify_data =  handshake_finished.get_verify_data(finished_key, finished_hash)
+        expected_verify_data = bytes.fromhex("""9b 9b 14 1d 90 63 37 fb d2 cb dc e7 1d f4
+                de da 4a b4 2c 30 95 72 cb 7f ff ee 54 54 b7 8f 07 18""")
+        self.assertEqual(verify_data, expected_verify_data)
