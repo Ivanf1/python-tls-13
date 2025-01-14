7dd90bd9f2f11c171ef59f9ceb67f228f1d0295b
Ivan Capobianco <capobianco.ivan@gmail.com>
2025-01-14 17:38:49 +0100

    feat: record application data message header

--- src/record_manager.py
+++ src/record_manager.py
@@ -3,9 +3,11 @@ from enum import Enum
 
 class RecordHeaderType(Enum):
     HANDSHAKE = 0
+    APPLICATION_DATA = 1
 
 record_header_type = {
     RecordHeaderType.HANDSHAKE : b'\x16',
+    RecordHeaderType.APPLICATION_DATA: b'\x17',
 }
 
 TLS_VERSION_10 = b'\x03\x01'
