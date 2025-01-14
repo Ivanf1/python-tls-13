a9bf1ec5632d73682ada0c807806f3ceed7177a5
Ivan Capobianco <capobianco.ivan@gmail.com>
2025-01-14 16:32:28 +0100

    feat: handshake finished complete

--- src/messages/handshake_finished.py
+++ src/messages/handshake_finished.py
@@ -7,3 +7,9 @@ class HandshakeFinished:
 
     def get_verify_data(self, finished_key, finished_hash):
         return get_hmac_sha256(finished_hash, finished_key)
+
+    def get_handshake_finished(self, finished_key, finished_hash):
+        verify_data = self.get_verify_data(finished_key, finished_hash)
+        verify_data_len = len(verify_data).to_bytes(3)
+
+        return self.HANDSHAKE_MESSAGE_TYPE_FINISHED + verify_data_len + verify_data
