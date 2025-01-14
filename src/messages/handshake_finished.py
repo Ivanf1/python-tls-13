d510b219db72309f21e9e93682ab5e16784ff4f1
Ivan Capobianco <capobianco.ivan@gmail.com>
2025-01-14 15:48:24 +0100

    feat: handshake finished message structure

---
+++ src/messages/handshake_finished.py
@@ -0,0 +1,3 @@
+class HandshakeFinished:
+    def __init__(self):
+        self.HANDSHAKE_MESSAGE_TYPE_FINISHED = b'\x14'
---
+++ test/test_handshake_finished.py
@@ -0,0 +1,4 @@
+import unittest
+
+class TestHandshakeFinished(unittest.TestCase):
+    pass
