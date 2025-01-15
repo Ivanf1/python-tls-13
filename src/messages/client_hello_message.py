0cd9582b535b3e0bb8b310859d734f479e29ae25
Ivan Capobianco <capobianco.ivan@gmail.com>
2025-01-15 13:32:57 +0100

    feat: client hello message class

---
+++ src/messages/client_hello_message.py
@@ -0,0 +1,29 @@
+from dataclasses import dataclass
+
+
+@dataclass
+class ClientHelloMessage:
+    handshake_message_type: bytes
+    bytes_of_client_hello_data: bytes
+    client_version: bytes
+    client_random: bytes
+    cipher_suites: bytes
+    extensions_length: bytes
+    extension_server_name: bytes
+    extension_supported_groups: bytes
+    extension_signature_algorithms: bytes
+    extension_supported_versions: bytes
+    extension_key_share: bytes
+
+    def to_bytes(self):
+        return self.handshake_message_type + \
+            self.bytes_of_client_hello_data + \
+            self.client_version + \
+            self.client_random + \
+            self.cipher_suites + \
+            self.extensions_length + \
+            self.extension_server_name + \
+            self.extension_supported_groups + \
+            self.extension_signature_algorithms + \
+            self.extension_supported_versions + \
+            self.extension_key_share
---
+++ test/test_client_hello_message.py
@@ -0,0 +1,36 @@
+import unittest
+
+from src.messages.client_hello_message import ClientHelloMessage
+
+
+class TestClientHelloMessage(unittest.TestCase):
+    def test_should_return_client_hello_message_bytes(self):
+        handshake_message_type = bytes.fromhex("01")
+        bytes_of_client_hello_data = bytes.fromhex("00007c")
+        client_version = bytes.fromhex("03 03")
+        client_random = bytes.fromhex("0000000000000000000000000000000000000000000000000000000000000000")
+        cipher_suites = bytes.fromhex("00021301")
+        extensions_length = bytes.fromhex("0054")
+        extension_server_name = bytes.fromhex("0000 0016 00 0013 6578616d706c652e756c666865696d2e6e6574")
+        extension_supported_groups = bytes.fromhex("000a 0002 001d")
+        extension_signature_algorithms = bytes.fromhex("000d 0002 0403")
+        extension_supported_versions = bytes.fromhex("002b 0002 0304")
+        extension_key_share = bytes.fromhex("""00330024001d00209a4407c27730168200ff65701c0f3f812b12a01df51b630351230280618a4067""")
+
+        client_hello_message = ClientHelloMessage(
+            handshake_message_type,
+            bytes_of_client_hello_data,
+            client_version,
+            client_random,
+            cipher_suites,
+            extensions_length,
+            extension_server_name,
+            extension_supported_groups,
+            extension_signature_algorithms,
+            extension_supported_versions,
+            extension_key_share,
+        ).to_bytes()
+
+        expected_client_hello_message = bytes.fromhex("""01 00007c 0303 0000000000000000000000000000000000000000000000000000000000000000 00021301 0054 0000 0016 00 0013 6578616d706c652e756c666865696d2e6e6574 000a 0002 001d 000d 0002 0403 002b 0002 0304 00330024001d00209a4407c27730168200ff65701c0f3f812b12a01df51b630351230280618a4067""")
+
+        self.assertEqual(client_hello_message, expected_client_hello_message)
