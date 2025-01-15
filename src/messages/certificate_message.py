7af2c7a167000afe0ae98f97e8bd07071ef54c0b
Ivan Capobianco <capobianco.ivan@gmail.com>
2025-01-15 15:20:49 +0100

    feat: certificate message

--- src/messages/certificate_message.py
+++ src/messages/certificate_message.py
@@ -1,36 +1,21 @@
+from dataclasses import dataclass
+
+
+@dataclass
 class CertificateMessage:
-    def __init__(self, certificate):
-        self.HANDSHAKE_MESSAGE_TYPE_CERTIFICATE = b'\x0b'
-        self.REQUEST_CONTEXT = b'\x00'
-        self.certificate = certificate
+    handshake_message_type: bytes
+    bytes_of_certificate_payload: bytes
+    request_context: bytes
+    certificates_length: bytes
+    certificate_length: bytes
+    certificate: bytes
+    certificate_extensions: bytes
 
-    def get_certificate_payload(self):
-        """
-        The certificate payload includes:\n
-        - Request Context \n
-        - Certificates Length \n
-        - Certificate Length \n
-        - Certificate \n
-        - Certificate Extensions \n
-
-        :return: certificate payload
-        """
-
-        certificate_len = len(self.certificate).to_bytes(3)
-        certificate_extensions = b'\x00\x00'
-
-        certificates_len = (len(certificate_len) + len(self.certificate) + len(certificate_extensions)).to_bytes(3)
-
-        return self.REQUEST_CONTEXT + \
-            certificates_len + \
-            certificate_len + \
+    def to_bytes(self):
+        return self.handshake_message_type + \
+            self.bytes_of_certificate_payload + \
+            self.request_context + \
+            self.certificates_length + \
+            self.certificate_length + \
             self.certificate + \
-            certificate_extensions
-
-    def get_certificate_message(self):
-        certificate_payload = self.get_certificate_payload()
-        certificate_payload_len = len(certificate_payload).to_bytes(3)
-
-        return self.HANDSHAKE_MESSAGE_TYPE_CERTIFICATE + \
-            certificate_payload_len + \
-            certificate_payload
+            self.certificate_extensions
---
+++ src/messages/certificate_message_builder.py
@@ -0,0 +1,36 @@
+class CertificateMessageBuilder:
+    def __init__(self, certificate):
+        self.HANDSHAKE_MESSAGE_TYPE_CERTIFICATE = b'\x0b'
+        self.REQUEST_CONTEXT = b'\x00'
+        self.certificate = certificate
+
+    def get_certificate_payload(self):
+        """
+        The certificate payload includes:\n
+        - Request Context \n
+        - Certificates Length \n
+        - Certificate Length \n
+        - Certificate \n
+        - Certificate Extensions \n
+
+        :return: certificate payload
+        """
+
+        certificate_len = len(self.certificate).to_bytes(3)
+        certificate_extensions = b'\x00\x00'
+
+        certificates_len = (len(certificate_len) + len(self.certificate) + len(certificate_extensions)).to_bytes(3)
+
+        return self.REQUEST_CONTEXT + \
+            certificates_len + \
+            certificate_len + \
+            self.certificate + \
+            certificate_extensions
+
+    def get_certificate_message(self):
+        certificate_payload = self.get_certificate_payload()
+        certificate_payload_len = len(certificate_payload).to_bytes(3)
+
+        return self.HANDSHAKE_MESSAGE_TYPE_CERTIFICATE + \
+            certificate_payload_len + \
+            certificate_payload
