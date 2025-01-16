e3b8415d28c1d795e6220c8f3311d40aef89c872
Ivan Capobianco <capobianco.ivan@gmail.com>
2025-01-16 16:50:32 +0100

    feat: start tls fsm on session start

--- src/tls_session.py
+++ src/tls_session.py
@@ -2,6 +2,7 @@ from src.messages.client_hello_message import ClientHelloMessage
 from src.messages.client_hello_message_builder import ClientHelloMessageBuilder
 from src.record_manager import RecordManager
 from src.tls_crypto import get_X25519_private_key, get_X25519_public_key
+from src.tls_fsm import TlsFsm, TlsFsmEvent
 from src.utils import TLSVersion, RecordHeaderType
 
 
@@ -12,12 +13,15 @@ class TlsSession:
         self.public_key = get_X25519_public_key(self.private_key)
 
         self.client_hello: ClientHelloMessage or None = None
+        self.tls_fsm = TlsFsm()
 
     def start(self) -> bytes:
         self.client_hello = ClientHelloMessageBuilder(
             self.server_name,
             self.public_key
         ).build_client_hello_message()
+
+        self.tls_fsm.transition(TlsFsmEvent.SESSION_BEGIN)
 
         return RecordManager.get_unencrypted_record(
             tls_version=TLSVersion.V1_0,
