7966000ad4668136f28d62fbd72fa1993ef21d44
Ivan Capobianco <capobianco.ivan@gmail.com>
2025-01-17 16:56:08 +0100

    feat: build handshake finished message from bytes

--- src/messages/handshake_finished_message_builder.py
+++ src/messages/handshake_finished_message_builder.py
@@ -18,3 +18,15 @@ class HandshakeFinishedMessageBuilder:
             verify_data_len,
             verify_data,
         )
+
+    @staticmethod
+    def build_from_bytes(message_bytes: bytes):
+        handshake_message_type = message_bytes[0:1]
+        bytes_of_handshake_data = message_bytes[1:4]
+        verify_data = message_bytes[4:]
+
+        return HandshakeFinishedMessage(
+            handshake_message_type=handshake_message_type,
+            bytes_of_handshake_data=bytes_of_handshake_data,
+            verify_data=verify_data
+        )
--- test/test_handshake_finished_message_builder.py
+++ test/test_handshake_finished_message_builder.py
@@ -4,6 +4,10 @@ from src.messages.handshake_finished_message_builder import HandshakeFinishedMes
 
 
 class TestHandshakeFinishedMessageBuilder(unittest.TestCase):
+    def setUp(self):
+        self.handshake_finished = bytes.fromhex("""14 00 00 20 9b 9b 14 1d 90 63 37 fb d2 cb
+         dc e7 1d f4 de da 4a b4 2c 30 95 72 cb 7f ff ee 54 54 b7 8f 07 18""")
+
     def test_should_return_verify_data(self):
         handshake_finished = HandshakeFinishedMessageBuilder()
         finished_key = bytes.fromhex("""00 8d 3b 66 f8 16 ea 55 9f 96 b5 37 e8 85
@@ -14,7 +18,7 @@ class TestHandshakeFinishedMessageBuilder(unittest.TestCase):
                 de da 4a b4 2c 30 95 72 cb 7f ff ee 54 54 b7 8f 07 18""")
         self.assertEqual(verify_data, expected_verify_data)
 
-    def test_should_return_handshake_finished_new(self):
+    def test_should_return_handshake_finished(self):
         handshake_finished = HandshakeFinishedMessageBuilder()
         finished_key = bytes.fromhex("""00 8d 3b 66 f8 16 ea 55 9f 96 b5 37 e8 85
                     c3 1f c0 68 bf 49 2c 65 2f 01 f2 88 a1 d8 cd c1 9f c8""")
@@ -23,3 +27,19 @@ class TestHandshakeFinishedMessageBuilder(unittest.TestCase):
         expected_finished = bytes.fromhex("""14 00 00 20 9b 9b 14 1d 90 63 37 fb d2 cb dc e7 1d f4
                 de da 4a b4 2c 30 95 72 cb 7f ff ee 54 54 b7 8f 07 18""")
         self.assertEqual(finished, expected_finished)
+
+    def test_should_build_handshake_finished_message_from_bytes_correct_handshake_message_type(self):
+        message = HandshakeFinishedMessageBuilder.build_from_bytes(self.handshake_finished)
+        expected_handshake_message_type = bytes.fromhex("14")
+        self.assertEqual(message.handshake_message_type, expected_handshake_message_type)
+
+    def test_should_build_handshake_finished_message_from_bytes_correct_bytes_fo_handshake_data(self):
+        message = HandshakeFinishedMessageBuilder.build_from_bytes(self.handshake_finished)
+        expected_bytes_of_handshake_data = bytes.fromhex("00 00 20")
+        self.assertEqual(message.bytes_of_handshake_data, expected_bytes_of_handshake_data)
+
+    def test_should_build_handshake_finished_message_from_bytes_correct_verify_data(self):
+        message = HandshakeFinishedMessageBuilder.build_from_bytes(self.handshake_finished)
+        expected_verify_data = bytes.fromhex("""9b 9b 14 1d 90 63 37 fb d2 cb
+         dc e7 1d f4 de da 4a b4 2c 30 95 72 cb 7f ff ee 54 54 b7 8f 07 18""")
+        self.assertEqual(message.verify_data, expected_verify_data)
