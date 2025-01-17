14e7039365a61547450cbee37155a5ae0b8f954a
Ivan Capobianco <capobianco.ivan@gmail.com>
2025-01-17 15:02:57 +0100

    feat: compute client handshake secret on server hello received

--- src/tls_session.py
+++ src/tls_session.py
@@ -1,10 +1,15 @@
+from cryptography.hazmat.primitives.asymmetric.x25519 import X25519PublicKey
+
 from src.messages.client_hello_message import ClientHelloMessage
 from src.messages.client_hello_message_builder import ClientHelloMessageBuilder
 from src.messages.server_hello_message import ServerHelloMessage
+from src.messages.server_hello_message_builder import ServerHelloMessageBuilder
 from src.record_manager import RecordManager
-from src.tls_crypto import get_X25519_private_key, get_X25519_public_key, get_early_secret, get_derived_secret
+from src.tls_crypto import get_X25519_private_key, get_X25519_public_key, get_early_secret, get_derived_secret, \
+    get_shared_secret, get_handshake_secret, get_records_hash_sha256, get_client_secret_handshake, \
+    get_client_handshake_key
 from src.tls_fsm import TlsFsm, TlsFsmEvent
-from src.utils import TLSVersion, RecordHeaderType
+from src.utils import TLSVersion, RecordHeaderType, HandshakeMessageType
 
 
 class TlsSession:
@@ -42,14 +47,31 @@ class TlsSession:
 
     def on_record_received(self, record):
         message_type = RecordManager.get_handshake_message_type(record)
-        self.tls_fsm.transition(message_type, record)
+
+        match message_type:
+            case HandshakeMessageType.SERVER_HELLO:
+                event = TlsFsmEvent.SERVER_HELLO_RECEIVED
+            case _:
+                event = None
+
+        self.tls_fsm.transition(event, record)
 
     def _on_session_begin_fsm_transaction(self, _):
         early_secret = get_early_secret()
         self.derived_secret = get_derived_secret(early_secret)
+        return True
 
     def _on_server_hello_received_fsm_transaction(self, ctx):
-        pass
+        self.server_hello = ServerHelloMessageBuilder.build_from_bytes(ctx[5:])
+        self.server_public_key = X25519PublicKey.from_public_bytes(self.server_hello.get_public_key())
+
+        shared_secret = get_shared_secret(self.private_key, self.server_public_key)
+        handshake_secret = get_handshake_secret(shared_secret, self.derived_secret)
+
+        hello_hash = get_records_hash_sha256(self.client_hello.to_bytes(), self.server_hello.to_bytes())
+
+        client_secret = get_client_secret_handshake(handshake_secret, hello_hash)
+        self.client_handshake_key = get_client_handshake_key(client_secret)
 
     def _on_certificate_received_fsm_transaction(self, ctx):
         pass
--- test/test_tls_session.py
+++ test/test_tls_session.py
@@ -1,9 +1,8 @@
 import unittest
 from unittest.mock import patch
 
-from src.tls_fsm import TlsFsm
+from src.tls_fsm import TlsFsm, TlsFsmEvent
 from src.tls_session import TlsSession
-from src.utils import HandshakeMessageType
 
 
 class TestTlsSession(unittest.TestCase):
@@ -25,7 +24,7 @@ class TestTlsSession(unittest.TestCase):
         with patch.object(TlsFsm, "transition") as mock_transition:
             session = TlsSession("example.com")
             session.on_record_received(record)
-            mock_transition.assert_called_with(HandshakeMessageType.SERVER_HELLO, record)
+            mock_transition.assert_called_with(TlsFsmEvent.SERVER_HELLO_RECEIVED, record)
 
     def test_should_compute_derived_secret_on_session_begin(self):
         self.tls_session.start()
@@ -33,3 +32,13 @@ class TestTlsSession(unittest.TestCase):
          b6 97 16 c0 76 18 9c 48 25 0c eb ea c3 57 6c 36 11 ba""")
         self.assertEqual(self.tls_session.derived_secret, expected_derived_secret)
 
+    def test_should_compute_client_handshake_key_on_server_hello_received(self):
+        self.tls_session.start()
+
+        server_hello = bytes.fromhex("""16 03 03 00 5a 02 00 00 56 03 03 a6 af 06 a4 12 18 60
+         dc 5e 6e 60 24 9c d3 4c 95 93 0c 8a c5 cb 14 34 da c1 55 77 2e
+         d3 e2 69 28 13 01 00 2e 00 2b 00 02 03 04 00 33 00 24 00 1d 00 
+         20 c9 82 88 76 11 20 95 fe 66 76 2b db f7 c6 72 e1 56 d6 cc 25 
+         3b 83 3d f1 dd 69 b1 b0 4e 75 1f 0f""")
+        self.tls_session.on_record_received(server_hello)
+        self.assertIs(len(self.tls_session.client_handshake_key), 16)
