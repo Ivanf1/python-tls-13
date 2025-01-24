bfbdf9f70c0d08e78d1d5622f6ebae0faaf0245f
Ivan Capobianco <capobianco.ivan@gmail.com>
2025-01-24 10:51:45 +0100

    fix: server fsm states

--- test/test_tls_server_fsm.py
+++ test/test_tls_server_fsm.py
@@ -11,10 +11,12 @@ class TestTlsClientFsm(unittest.TestCase):
         self.tls_events = [event for event in TlsServerFsmEvent]
 
         self.on_session_begin_transaction_cb = Mock(return_value=True)
+        self.on_client_hello_received_cb = Mock(return_value=True)
         self.on_finished_received_cb = Mock(return_value=True)
 
         self.tls_fsm = TlsServerFsm(
             self.on_session_begin_transaction_cb,
+            self.on_client_hello_received_cb,
             self.on_finished_received_cb
         )
 
@@ -24,9 +26,9 @@ class TestTlsClientFsm(unittest.TestCase):
     def test_should_return_tls_events(self):
         self.assertSequenceEqual(self.tls_fsm.get_events(), self.tls_events)
 
-    def test_should_proceed_to_wait_finished_state(self):
+    def test_should_proceed_to_wait_client_hello_state(self):
         self.tls_fsm.transition(TlsServerFsmEvent.SESSION_BEGIN)
-        self.assertEqual(self.tls_fsm.get_current_state(), TlsServerFsmState.WAIT_FINISHED)
+        self.assertEqual(self.tls_fsm.get_current_state(), TlsServerFsmState.WAIT_CLIENT_HELLO)
 
     def test_should_call_on_session_begin_cb_with_context(self):
         ctx = "sb ctx"
@@ -36,13 +38,26 @@ class TestTlsClientFsm(unittest.TestCase):
     def test_should_not_proceed_to_next_state_if_event_invalid_for_current_state(self):
         self.assertRaises(FSMInvalidEventForStateError, self.tls_fsm.transition, TlsServerFsmEvent.FINISHED_RECEIVED)
 
+    def test_should_proceed_to_wait_finished_state(self):
+        self.tls_fsm.transition(TlsServerFsmEvent.SESSION_BEGIN)
+        self.tls_fsm.transition(TlsServerFsmEvent.CLIENT_HELLO_RECEIVED)
+        self.assertEqual(self.tls_fsm.get_current_state(), TlsServerFsmState.WAIT_FINISHED)
+
+    def test_should_call_on_client_hello_received_with_context(self):
+        ctx = "chr ctx"
+        self.tls_fsm.transition(TlsServerFsmEvent.SESSION_BEGIN)
+        self.tls_fsm.transition(TlsServerFsmEvent.CLIENT_HELLO_RECEIVED, ctx)
+        self.on_client_hello_received_cb.assert_called_with(ctx)
+
     def test_should_proceed_to_connected_state(self):
         self.tls_fsm.transition(TlsServerFsmEvent.SESSION_BEGIN)
+        self.tls_fsm.transition(TlsServerFsmEvent.CLIENT_HELLO_RECEIVED)
         self.tls_fsm.transition(TlsServerFsmEvent.FINISHED_RECEIVED)
         self.assertEqual(self.tls_fsm.get_current_state(), TlsServerFsmState.CONNECTED)
 
     def test_should_call_on_finished_received_with_context(self):
         ctx = "fr ctx"
         self.tls_fsm.transition(TlsServerFsmEvent.SESSION_BEGIN)
+        self.tls_fsm.transition(TlsServerFsmEvent.CLIENT_HELLO_RECEIVED)
         self.tls_fsm.transition(TlsServerFsmEvent.FINISHED_RECEIVED, ctx)
         self.on_finished_received_cb.assert_called_with(ctx)
