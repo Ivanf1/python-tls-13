7e190f98e65441d295cf380c7c908908e7631e04
Ivan Capobianco <capobianco.ivan@gmail.com>
2025-01-24 13:04:01 +0100

    feat: build server hello on client hello received

--- src/tls_server_session.py
+++ src/tls_server_session.py
@@ -1,5 +1,11 @@
+from src.messages.client_hello_message import ClientHelloMessage
+from src.messages.client_hello_message_builder import ClientHelloMessageBuilder
+from src.messages.server_hello_message import ServerHelloMessage
+from src.messages.server_hello_message_builder import ServerHelloMessageBuilder
+from src.record_manager import RecordManager
 from src.tls_crypto import get_X25519_private_key, get_X25519_public_key
 from src.tls_server_fsm import TlsServerFsm, TlsServerFsmEvent
+from src.utils import HandshakeMessageType
 
 
 class TlsServerSession:
@@ -8,20 +14,39 @@ class TlsServerSession:
         self.public_key = get_X25519_public_key(self.private_key)
         self.on_data_to_send = on_data_to_send
 
+        self.client_hello: ClientHelloMessage or None = None
+        self.server_hello: ServerHelloMessage or None = None
+
         self.tls_fsm = TlsServerFsm(
             on_session_begin_transaction_cb=self._on_session_begin_fsm_transaction,
             on_client_hello_received_transaction_cb=self._on_client_hello_received_fsm_transaction,
             on_finished_received_cb=self._on_finished_received_fsm_transaction
         )
 
+    def on_record_received(self, record):
+        message_type = RecordManager.get_handshake_message_type(record)
+
+        match message_type:
+            case HandshakeMessageType.CLIENT_HELLO:
+                event = TlsServerFsmEvent.CLIENT_HELLO_RECEIVED
+            case _:
+                event = None
+
+        self.tls_fsm.transition(event, record)
+
     def start(self):
         self.tls_fsm.transition(TlsServerFsmEvent.SESSION_BEGIN)
 
     def _on_session_begin_fsm_transaction(self, ctx):
-        pass
+        return True
 
     def _on_client_hello_received_fsm_transaction(self, ctx):
-        pass
+        self.client_hello = ClientHelloMessageBuilder.build_from_bytes(ctx[5:])
+        self.server_hello = self._build_server_hello()
+        return True
 
     def _on_finished_received_fsm_transaction(self, ctx):
         pass
+
+    def _build_server_hello(self):
+        return ServerHelloMessageBuilder(self.public_key).build_server_hello_message()
--- test/test_tls_server_session.py
+++ test/test_tls_server_session.py
@@ -3,12 +3,24 @@ from unittest.mock import patch, Mock
 
 from src.tls_server_fsm import TlsServerFsm, TlsServerFsmEvent
 from src.tls_server_session import TlsServerSession
+from src.utils import HandshakeMessageType
 
 
 class TestTlsServerSession(unittest.TestCase):
+    def setUp(self):
+        self.client_hello = bytes.fromhex("""17 03 03 01 02 01 00 00 f4 03 03 00 01 02 03 04 05 06 07 08 09 0a 0b 0c 0d 0e 0f 10 11 
+        12 13 14 15 16 17 18 19 1a 1b 1c 1d 1e 1f 00 08 13 01 00 a3 00 00 00 18 00 16 00 00 13 65 78 61 6d 70 6c 65 2e 
+        75 6c 66 68 65 69 6d 2e 6e 65 74 00 0a 00 16 00 1d 00 0d 00 1e 04 03 00 2b 03 04 00 33 00 24 00 1d 00 20 35 80 
+        72 d6 36 58 80 d1 ae ea 32 9a df 91 21 38 38 51 ed 21 a2 8e 3b 75 e9 65 d0 d2 cd 16 62 54""")
 
     def test_should_call_transition_on_session_begin(self):
         with patch.object(TlsServerFsm, "transition") as mock_transition:
             session = TlsServerSession(Mock())
             session.start()
             mock_transition.assert_called_with(TlsServerFsmEvent.SESSION_BEGIN)
+
+    def test_should_build_server_hello_on_client_hello_received(self):
+        session = TlsServerSession(Mock())
+        session.start()
+        session.on_record_received(self.client_hello)
+        self.assertEqual(session.server_hello.to_bytes()[0:1], HandshakeMessageType.SERVER_HELLO.value)
