da55f5e9bf8c5dc4ef506dbd572fbc4f0f8ac77e
Ivan Capobianco <capobianco.ivan@gmail.com>
2025-01-25 17:00:33 +0100

    fix: server fsm states and events

--- src/tls_server_fsm.py
+++ src/tls_server_fsm.py
@@ -5,25 +5,29 @@ from src.fsm import FSM
 
 class TlsServerFsmState(Enum):
     START = 0,
-    WAIT_FINISHED = 1,
-    CONNECTED = 2
+    WAIT_CLIENT_HELLO = 1,
+    WAIT_FINISHED = 2,
+    CONNECTED = 3
 
 class TlsServerFsmEvent(Enum):
     SESSION_BEGIN = 0,
-    FINISHED_RECEIVED = 1
+    CLIENT_HELLO_RECEIVED = 1,
+    FINISHED_RECEIVED = 2
 
 
 class TlsServerFsm(FSM):
     def __init__(
             self,
-            on_session_begin_transaction_cb = None,
-            on_finished_received_cb = None
+            on_session_begin_transaction_cb=None,
+            on_client_hello_received_transaction_cb=None,
+            on_finished_received_cb=None
     ):
         tls_states = [state for state in TlsServerFsmState]
         self.events = [event for event in TlsServerFsmEvent]
 
         tls_table = {
-            (TlsServerFsmState.START, TlsServerFsmEvent.SESSION_BEGIN): (TlsServerFsmState.WAIT_FINISHED, on_session_begin_transaction_cb),
+            (TlsServerFsmState.START, TlsServerFsmEvent.SESSION_BEGIN): (TlsServerFsmState.WAIT_CLIENT_HELLO, on_session_begin_transaction_cb),
+            (TlsServerFsmState.WAIT_CLIENT_HELLO, TlsServerFsmEvent.CLIENT_HELLO_RECEIVED): (TlsServerFsmState.WAIT_FINISHED, on_client_hello_received_transaction_cb),
             (TlsServerFsmState.WAIT_FINISHED, TlsServerFsmEvent.FINISHED_RECEIVED): (TlsServerFsmState.CONNECTED, on_finished_received_cb),
         }
 
