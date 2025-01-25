410675c0cbf18f472f077b6c5d1ef7f9d9f0d2f4
Ivan Capobianco <capobianco.ivan@gmail.com>
2025-01-25 18:44:35 +0100

    refactor: certificate verify builder

--- test/test_certificate_verify_message_builder.py
+++ test/test_certificate_verify_message_builder.py
@@ -16,8 +16,7 @@ class TestCertificateVerifyMessageBuilder(unittest.TestCase):
 
     def test_should_return_certificate_verify_message(self):
         m = CertificateVerifyMessageBuilder(f"../test/data/private_key.pem")
-        private_key_path = path.join(path.dirname(path.abspath(__file__)), "data", "private_key.pem")
-        certificate_verify_message = m.get_certificate_verify_message(b'', private_key_path).to_bytes()
+        certificate_verify_message = m.get_certificate_verify_message(b'').to_bytes()
         certificate_verify_message_first_bytes = certificate_verify_message[:8]
         expected_certificate_verify_message_first_bytes = bytes.fromhex("""0f 00 01 04 08 04 01 00""")
         self.assertEqual(certificate_verify_message_first_bytes, expected_certificate_verify_message_first_bytes)
