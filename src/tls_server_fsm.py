220f410331e019ec3ee6d71a829d04e143366cf1
Ivan Capobianco <capobianco.ivan@gmail.com>
2025-01-26 09:26:38 +0100

    feat: tls server fsm client auth

--- src/tls_server_fsm.py
+++ src/tls_server_fsm.py
@@ -6,35 +6,51 @@ from src.fsm import FSM
 class TlsServerFsmState(Enum):
     START = 0,
     WAIT_CLIENT_HELLO = 1,
-    WAIT_FINISHED = 2,
-    CONNECTED = 3
+    WAIT_CERTIFICATE = 2,
+    WAIT_CERTIFICATE_VERIFY = 3,
+    WAIT_FINISHED = 4,
+    CONNECTED = 5
 
 class TlsServerFsmEvent(Enum):
     SESSION_BEGIN = 0,
     CLIENT_HELLO_RECEIVED = 1,
-    FINISHED_RECEIVED = 2
+    CERTIFICATE_RECEIVED = 2,
+    CERTIFICATE_VERIFY_RECEIVED = 3,
+    FINISHED_RECEIVED = 4
 
 
 class TlsServerFsm(FSM):
     def __init__(
             self,
+            client_authentication=False,
             on_session_begin_transaction_cb=None,
             on_client_hello_received_transaction_cb=None,
+            on_client_certificate_received_transaction_cb=None,
+            on_client_certificate_verify_received_transaction_cb=None,
             on_finished_received_cb=None
     ):
         tls_states = [state for state in TlsServerFsmState]
         self.events = [event for event in TlsServerFsmEvent]
 
-        tls_table = {
-            (TlsServerFsmState.START, TlsServerFsmEvent.SESSION_BEGIN): (TlsServerFsmState.WAIT_CLIENT_HELLO, on_session_begin_transaction_cb),
-            (TlsServerFsmState.WAIT_CLIENT_HELLO, TlsServerFsmEvent.CLIENT_HELLO_RECEIVED): (TlsServerFsmState.WAIT_FINISHED, on_client_hello_received_transaction_cb),
-            (TlsServerFsmState.WAIT_FINISHED, TlsServerFsmEvent.FINISHED_RECEIVED): (TlsServerFsmState.CONNECTED, on_finished_received_cb),
-        }
+        if client_authentication:
+            self.tls_table = {
+                (TlsServerFsmState.START, TlsServerFsmEvent.SESSION_BEGIN): (TlsServerFsmState.WAIT_CLIENT_HELLO, on_session_begin_transaction_cb),
+                (TlsServerFsmState.WAIT_CLIENT_HELLO, TlsServerFsmEvent.CLIENT_HELLO_RECEIVED): (TlsServerFsmState.WAIT_CERTIFICATE, on_client_hello_received_transaction_cb),
+                (TlsServerFsmState.WAIT_CERTIFICATE, TlsServerFsmEvent.CERTIFICATE_RECEIVED): (TlsServerFsmState.WAIT_CERTIFICATE_VERIFY, on_client_certificate_received_transaction_cb),
+                (TlsServerFsmState.WAIT_CERTIFICATE_VERIFY, TlsServerFsmEvent.CERTIFICATE_VERIFY_RECEIVED): (TlsServerFsmState.WAIT_FINISHED, on_client_certificate_verify_received_transaction_cb),
+                (TlsServerFsmState.WAIT_FINISHED, TlsServerFsmEvent.FINISHED_RECEIVED): (TlsServerFsmState.CONNECTED, on_finished_received_cb),
+            }
+        else:
+            self.tls_table = {
+                (TlsServerFsmState.START, TlsServerFsmEvent.SESSION_BEGIN): (TlsServerFsmState.WAIT_CLIENT_HELLO, on_session_begin_transaction_cb),
+                (TlsServerFsmState.WAIT_CLIENT_HELLO, TlsServerFsmEvent.CLIENT_HELLO_RECEIVED): (TlsServerFsmState.WAIT_FINISHED, on_client_hello_received_transaction_cb),
+                (TlsServerFsmState.WAIT_FINISHED, TlsServerFsmEvent.FINISHED_RECEIVED): (TlsServerFsmState.CONNECTED, on_finished_received_cb),
+            }
 
         super().__init__(
             states=tls_states,
             initial_state=tls_states[0],
-            table=tls_table,
+            table=self.tls_table,
         )
 
     def get_states(self):
--- test/test_tls_server_fsm.py
+++ test/test_tls_server_fsm.py
@@ -11,13 +11,24 @@ class TestTlsServerFsm(unittest.TestCase):
         self.tls_events = [event for event in TlsServerFsmEvent]
 
         self.on_session_begin_transition_cb = Mock(return_value=True)
+        self.on_client_certificate_received_transaction_cb = Mock(return_value=True)
+        self.on_client_certificate_verify_received_transaction_cb = Mock(return_value=True)
         self.on_client_hello_received_transition_cb = Mock(return_value=True)
         self.on_finished_received_cb = Mock(return_value=True)
 
         self.tls_fsm = TlsServerFsm(
-            self.on_session_begin_transition_cb,
-            self.on_client_hello_received_transition_cb,
-            self.on_finished_received_cb
+            on_session_begin_transaction_cb=self.on_session_begin_transition_cb,
+            on_client_hello_received_transaction_cb=self.on_client_hello_received_transition_cb,
+            on_finished_received_cb=self.on_finished_received_cb
+        )
+
+        self.tls_fsm_client_auth = TlsServerFsm(
+            client_authentication=True,
+            on_session_begin_transaction_cb=self.on_session_begin_transition_cb,
+            on_client_hello_received_transaction_cb=self.on_client_hello_received_transition_cb,
+            on_client_certificate_received_transaction_cb=self.on_client_certificate_received_transaction_cb,
+            on_client_certificate_verify_received_transaction_cb=self.on_client_certificate_verify_received_transaction_cb,
+            on_finished_received_cb=self.on_finished_received_cb
         )
 
     def test_should_return_tls_states(self):
@@ -60,4 +71,60 @@ class TestTlsServerFsm(unittest.TestCase):
         self.tls_fsm.transition(TlsServerFsmEvent.SESSION_BEGIN)
         self.tls_fsm.transition(TlsServerFsmEvent.CLIENT_HELLO_RECEIVED)
         self.tls_fsm.transition(TlsServerFsmEvent.FINISHED_RECEIVED, ctx)
+        self.on_finished_received_cb.assert_called_with(ctx)
+
+    def test_should_proceed_to_wait_certificate_state(self):
+        self.tls_fsm_client_auth.transition(TlsServerFsmEvent.SESSION_BEGIN)
+        self.tls_fsm_client_auth.transition(TlsServerFsmEvent.CLIENT_HELLO_RECEIVED)
+        self.assertEqual(self.tls_fsm_client_auth.get_current_state(), TlsServerFsmState.WAIT_CERTIFICATE)
+
+    def test_should_call_on_client_hello_received_with_context_when_client_auth(self):
+        ctx = "chr ctx"
+        self.tls_fsm_client_auth.transition(TlsServerFsmEvent.SESSION_BEGIN)
+        self.tls_fsm_client_auth.transition(TlsServerFsmEvent.CLIENT_HELLO_RECEIVED, ctx)
+        self.on_client_hello_received_transition_cb.assert_called_with(ctx)
+
+    def test_should_proceed_to_wait_certificate_verify_state(self):
+        self.tls_fsm_client_auth.transition(TlsServerFsmEvent.SESSION_BEGIN)
+        self.tls_fsm_client_auth.transition(TlsServerFsmEvent.CLIENT_HELLO_RECEIVED)
+        self.tls_fsm_client_auth.transition(TlsServerFsmEvent.CERTIFICATE_RECEIVED)
+        self.assertEqual(self.tls_fsm_client_auth.get_current_state(), TlsServerFsmState.WAIT_CERTIFICATE_VERIFY)
+
+    def test_should_call_on_certificate_received_with_context(self):
+        ctx = "cr ctx"
+        self.tls_fsm_client_auth.transition(TlsServerFsmEvent.SESSION_BEGIN)
+        self.tls_fsm_client_auth.transition(TlsServerFsmEvent.CLIENT_HELLO_RECEIVED)
+        self.tls_fsm_client_auth.transition(TlsServerFsmEvent.CERTIFICATE_RECEIVED, ctx)
+        self.on_client_certificate_received_transaction_cb.assert_called_with(ctx)
+
+    def test_should_proceed_to_wait_finished_state_when_client_auth(self):
+        self.tls_fsm_client_auth.transition(TlsServerFsmEvent.SESSION_BEGIN)
+        self.tls_fsm_client_auth.transition(TlsServerFsmEvent.CLIENT_HELLO_RECEIVED)
+        self.tls_fsm_client_auth.transition(TlsServerFsmEvent.CERTIFICATE_RECEIVED)
+        self.tls_fsm_client_auth.transition(TlsServerFsmEvent.CERTIFICATE_VERIFY_RECEIVED)
+        self.assertEqual(self.tls_fsm_client_auth.get_current_state(), TlsServerFsmState.WAIT_FINISHED)
+
+    def test_should_call_on_certificate_verify_received_with_context(self):
+        ctx = "cvr ctx"
+        self.tls_fsm_client_auth.transition(TlsServerFsmEvent.SESSION_BEGIN)
+        self.tls_fsm_client_auth.transition(TlsServerFsmEvent.CLIENT_HELLO_RECEIVED)
+        self.tls_fsm_client_auth.transition(TlsServerFsmEvent.CERTIFICATE_RECEIVED)
+        self.tls_fsm_client_auth.transition(TlsServerFsmEvent.CERTIFICATE_VERIFY_RECEIVED, ctx)
+        self.on_client_certificate_verify_received_transaction_cb.assert_called_with(ctx)
+
+    def test_should_proceed_to_connected_state_when_client_auth(self):
+        self.tls_fsm_client_auth.transition(TlsServerFsmEvent.SESSION_BEGIN)
+        self.tls_fsm_client_auth.transition(TlsServerFsmEvent.CLIENT_HELLO_RECEIVED)
+        self.tls_fsm_client_auth.transition(TlsServerFsmEvent.CERTIFICATE_RECEIVED)
+        self.tls_fsm_client_auth.transition(TlsServerFsmEvent.CERTIFICATE_VERIFY_RECEIVED)
+        self.tls_fsm_client_auth.transition(TlsServerFsmEvent.FINISHED_RECEIVED)
+        self.assertEqual(self.tls_fsm_client_auth.get_current_state(), TlsServerFsmState.CONNECTED)
+
+    def test_should_call_on_finished_received_with_context_when_client_auth(self):
+        ctx = "fr ctx"
+        self.tls_fsm_client_auth.transition(TlsServerFsmEvent.SESSION_BEGIN)
+        self.tls_fsm_client_auth.transition(TlsServerFsmEvent.CLIENT_HELLO_RECEIVED)
+        self.tls_fsm_client_auth.transition(TlsServerFsmEvent.CERTIFICATE_RECEIVED)
+        self.tls_fsm_client_auth.transition(TlsServerFsmEvent.CERTIFICATE_VERIFY_RECEIVED)
+        self.tls_fsm_client_auth.transition(TlsServerFsmEvent.FINISHED_RECEIVED, ctx)
         self.on_finished_received_cb.assert_called_with(ctx)
